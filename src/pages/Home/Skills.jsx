5c3e2c1eebb70a0c96e37d4881e91d652d4c0fa1
ii-shimul <islamshimul27@gmail.com>
2025-02-07 02:57:35 +0600

    made improvements

--- src/pages/Home/Skills.jsx
+++ src/pages/Home/Skills.jsx
@@ -1,8 +1,4 @@
 import { motion } from "framer-motion";
-import { Tooltip } from "react-tooltip";
-import "react-tooltip/dist/react-tooltip.css";
-import { CircularProgressbarWithChildren } from "react-circular-progressbar";
-import "react-circular-progressbar/dist/styles.css";
 import {
   FaHtml5,
   FaCss3Alt,
@@ -14,27 +10,23 @@ import {
 import { SiTailwindcss, SiMongodb } from "react-icons/si";
 
 const skills = [
-  { name: "HTML", icon: <FaHtml5 />, color: "#E44D26", level: 90 },
-  { name: "CSS", icon: <FaCss3Alt />, color: "#1572B6", level: 85 },
-  { name: "JavaScript", icon: <FaJs />, color: "#F7DF1E", level: 80 },
-  { name: "React", icon: <FaReact />, color: "#61DAFB", level: 75 },
-  { name: "Node.js", icon: <FaNodeJs />, color: "#339933", level: 80 },
+  { name: "HTML", icon: <FaHtml5 />, color: "#E44D26" },
+  { name: "CSS", icon: <FaCss3Alt />, color: "#1572B6" },
+  { name: "JavaScript", icon: <FaJs />, color: "#F7DF1E" },
+  { name: "React", icon: <FaReact />, color: "#61DAFB" },
+  { name: "Node.js", icon: <FaNodeJs />, color: "#339933" },
   {
     name: "Tailwind CSS",
     icon: <SiTailwindcss />,
     color: "#06B6D4",
-    level: 75,
   },
-  { name: "MongoDB", icon: <SiMongodb />, color: "#47A248", level: 70 },
-  { name: "Git", icon: <FaGitAlt />, color: "#F05032", level: 85 },
+  { name: "MongoDB", icon: <SiMongodb />, color: "#47A248" },
+  { name: "Git", icon: <FaGitAlt />, color: "#F05032" },
 ];
 
 const Skills = () => {
   return (
-    <div
-      id="skills"
-      className="max-w-7xl mx-auto mt-10 max-sm:px-10 pb-10"
-    >
+    <div id="skills" className="max-w-6xl mx-auto mt-10 max-sm:px-10 pb-10">
       <motion.h2
         className="text-4xl font-bold text-gray-800 dark:text-white text-center mb-12"
         initial={{ opacity: 0, y: -50 }}
@@ -44,11 +36,11 @@ const Skills = () => {
         My Skills
       </motion.h2>
 
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-7 lg:gap-10">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-5 lg:gap-7">
         {skills.map((skill, index) => (
           <motion.div
             key={skill.name}
-            className="group"
+            className="group "
             initial={{ opacity: 0, scale: 0.8 }}
             whileInView={{ opacity: 1, scale: 1 }}
             whileHover={{ scale: 1.1 }}
@@ -59,33 +51,18 @@ const Skills = () => {
               ease: "easeInOut",
             }}
           >
-            {/* Circular Progress Bar */}
-            <CircularProgressbarWithChildren
-              value={skill.level}
-              styles={{
-                path: { stroke: skill.color, strokeWidth: 6 },
-                trail: { stroke: "#eee", strokeWidth: 6 },
-              }}
-              className=""
+            <div
+              className={`flex items-center justify-center text-6xl text-gray-700 dark:text-gray-100 hover:text-[${skill.color}]`}
+              onMouseEnter={(e) => (e.currentTarget.style.color = skill.color)}
+              onMouseLeave={(e) => (e.currentTarget.style.color = "")}
             >
-              <div className="text-7xl text-gray-700 dark:text-gray-100">
-                {skill.icon}
-              </div>
-            </CircularProgressbarWithChildren>
+              {skill.icon}
+            </div>
 
             {/* Skill Name */}
             <h3 className="text-center mt-4 text-lg font-medium text-gray-700 dark:text-gray-200">
               {skill.name}
             </h3>
-
-            {/* Tooltip */}
-            <div
-              id={`tooltip-${skill.name}`}
-              className="absolute bottom-20 hidden group-hover:block z-50 text-sm p-2 bg-gray-800 text-white rounded shadow-md"
-              style={{ transform: "translateY(110%)" }}
-            >
-              Proficiency: {skill.level}%
-            </div>
           </motion.div>
         ))}
       </div>
