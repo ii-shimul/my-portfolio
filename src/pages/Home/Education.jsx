f2bb4ae85875a3fb70b493c708e8abcfe518db14
bax <islamshimul27@gmail.com>
2025-08-01 02:05:31 +0600

    updated the cgpa

--- src/pages/Home/Education.jsx
+++ src/pages/Home/Education.jsx
@@ -1,30 +1,30 @@
 const Education = () => {
-  return (
-    <div className="bg-inherit">
-      <h3 className="text-xl md:text-2xl text-center font-bold text-gray-800 dark:text-white mb-3 md:mb-6">
-        My educational background
-      </h3>
+	return (
+		<div className="bg-inherit">
+			<h3 className="text-xl md:text-2xl text-center font-bold text-gray-800 dark:text-white mb-3 md:mb-6">
+				My educational background
+			</h3>
 
-      <p className="text-sm md:text-base text-gray-800 dark:text-gray-200 mb-2">
-        <strong>Degree:</strong> B.Sc. in Computer Science & Engineering
-      </p>
-      <p className="text-sm md:text-base text-gray-800 dark:text-gray-200 mb-2">
-        <strong>University:</strong> Leading University, Sylhet
-      </p>
-      <p className="text-sm md:text-base text-gray-800 dark:text-gray-200 mb-2">
-        <strong>Semester:</strong> 3rd Semester
-      </p>
-      <p className="text-sm md:text-base text-gray-800 dark:text-gray-200 mb-4">
-        <strong>CGPA:</strong> 3.95
-      </p>
-      <p className="text-gray-700 text-sm md:text-base dark:text-gray-300">
-        As a dedicated Computer Science student, I am passionate about exploring
-        web development, programming, and solving real-world problems through
-        technology. My strong academic background reflects my commitment to
-        learning and excelling in the tech field.
-      </p>
-    </div>
-  );
+			<p className="text-sm md:text-base text-gray-800 dark:text-gray-200 mb-2">
+				<strong>Degree:</strong> B.Sc. in Computer Science & Engineering
+			</p>
+			<p className="text-sm md:text-base text-gray-800 dark:text-gray-200 mb-2">
+				<strong>University:</strong> Leading University, Sylhet
+			</p>
+			<p className="text-sm md:text-base text-gray-800 dark:text-gray-200 mb-2">
+				<strong>Semester:</strong> 3rd Semester
+			</p>
+			<p className="text-sm md:text-base text-gray-800 dark:text-gray-200 mb-4">
+				<strong>CGPA:</strong> 3.69
+			</p>
+			<p className="text-gray-700 text-sm md:text-base dark:text-gray-300">
+				As a dedicated Computer Science student, I am passionate about exploring
+				web development, programming, and solving real-world problems through
+				technology. My strong academic background reflects my commitment to
+				learning and excelling in the tech field.
+			</p>
+		</div>
+	);
 };
 
 export default Education;
